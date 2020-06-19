22adabfe78c5188478369bff7ef9e61e6bcf16b7
CraigCraigCraig <CraigCraigCraig>
2020-06-18 21:09:16 -0700

    initial commit

---
+++ react-front-end-src/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { TemplateForm } from "./components/TemplateForm";
+
+function App() {
+  return (
+    <div className="App">
+      <TemplateForm />
+    </div>
+  );
+}
+
+export default App;
